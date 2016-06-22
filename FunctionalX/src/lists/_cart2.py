f251c6fe303b26032582b02ee97d7cacb05e3f20
Yuhang(Steven) Wang <me@me>
2016-06-22 17:05:17 -0500

    update pylintrc options

--- FunctionalX/src/lists/_cart2.py
+++ FunctionalX/src/lists/_cart2.py
@@ -8,14 +8,14 @@ def cart2(list1: list, list2: list) -> list:
         the two lists.
     :rtype: list
     
-    >>> cart2(['a','b'], [1,2]) 
+    >>> cart2(['a','b'], [1,2])
     [['a',1],['a',2],['b',1], ['b',2]]
     """
     def aux(list1: list, list2: list, accum: list) -> list:
         if len(list1) == 0 or len(list2) == 0: # base case
-            return accum 
-        elif len(list1) == 1: # start to traverse list2 
+            return accum
+        elif len(list1) == 1: # start to traverse list2
             return aux(list1, list2[1:], accum + [[list1[0], list2[0]]])
         else:
             return aux(list1[1:], list2, aux(list1[0:1], list2, accum))
-    return aux(list1, list2,[])
+    return aux(list1, list2, [])
