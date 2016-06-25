0cf751f392fe170127e6302f89a1962b961afff3
Yuhang(Steven) Wang <me@me>
2016-06-25 15:19:14 -0500

    add fix test messages for cartn

--- FunctionalX/tests/lists/cartn/test_1.py
+++ FunctionalX/tests/lists/cartn/test_1.py
@@ -5,7 +5,7 @@ def run():
     print(tests.dashed_line(80,'-'))
     result   = lists.cartn(['a','b'], [1,2], ['A','B'])
     solution = [['a', 1, 'A'], ['a', 1, 'B'], ['a', 2, 'A'], ['a', 2, 'B'], ['b', 1, 'A'], ['b', 1, 'B'], ['b', 2, 'A'], ['b', 2, 'B']]
-    assert result == solution, "ERROR HINT: Test 1 for lists.cart_append() failed"
+    assert result == solution, "ERROR HINT: Test 1 for lists.cartn() failed"
     print("Result = ", result,"\n")
-    print("lists.cart_append(): test 1 passed!")
+    print("lists.cartn(): test 1 passed!")
     print(tests.dashed_line(80,'-'))
